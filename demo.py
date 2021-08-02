c2447d2f24b24a0000a074bc33d7d75ef13cf8e5
Gowtham S <32368731+GowthamS07@users.noreply.github.com>
2021-08-02 21:54:05 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 21:45:01 2021
+
+@author: Gowtham S
+"""
+
+'''
+What is a code ??
+
+Step by Step instructions
+
+Comment
+'''
+
+result = 1 + 2  # result is a variable
+
+
